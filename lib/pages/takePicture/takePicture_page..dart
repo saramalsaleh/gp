8f7f77d81d03a0868abf9b4b42d527cd992f2f31
saramalsaleh <114119106+saramalsaleh@users.noreply.github.com>
2024-05-07 01:42:30 +0300

    bug fixed

--- lib/pages/takePicture/takePicture_page..dart
+++ lib/pages/takePicture/takePicture_page..dart
@@ -28,122 +28,126 @@ class _TakePictureAiPageState extends State<TakePictureAiPage> {
 
   @override
   Widget build(BuildContext context) {
-    return AppBackGround(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.arrow_back_ios_new_outlined)),
-                const StarCount(),
-              ],
-            ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InkWell(
-                    onTap: () async {
-                      FlutterTts().speak(controller.answer);
-                    },
-                    child: Center(
-                      child: Text(
-                        controller.answer,
-                        style: TextStyle(
-                          fontSize: 44,
-                          fontFamily: 'ribeye',
-                          color: controller.getColor(),
-                          shadows: textShadow,
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      controller.pickImage();
-                    },
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        ShadowWidget(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: Colors.grey.shade300,
+    return ValueListenableBuilder(
+      builder: (context,_,__) {
+        return AppBackGround(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(Icons.arrow_back_ios_new_outlined)),
+                    const StarCount(),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () async {
+                          FlutterTts().speak(controller.answer);
+                        },
+                        child: Center(
+                          child: Text(
+                            controller.answer,
+                            style: TextStyle(
+                              fontSize: 44,
+                              fontFamily: 'ribeye',
+                              color: controller.getColor(),
+                              shadows: textShadow,
                             ),
-                            height: 300,
-                            width: 300,
-                            child: (controller.imagePath == null)
-                                ? null
-                                : Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(8.0),
-                                        child: Image.file(
-                                          File(controller.imagePath!),
-                                          height: 280,
-                                          width: 280,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
                           ),
                         ),
-                        Icon(
-                          Icons.camera_enhance_rounded,
-                          color: AppColor.tealColor.withOpacity(
-                              controller.imagePath != null ? 0.6 : 1),
-                          size: 120,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 42),
-                    child: AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 300),
-                      child: controller.imagePath == null
-                          ? const SizedBox()
-                          : InkWell(
-                              onTap: () {
-                                controller.showResult(context);
-                              },
-                              child: const ShadowWidget.image(
-                                child: IgnorePointer(
-                                  child: AppButton(
-                                    title: 'CONTINUE',
-                                    icon: Padding(
-                                      padding: EdgeInsets.only(top: 6),
-                                      child: Icon(
-                                        Icons.arrow_forward_rounded,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    backgroundColor: AppColor.pinkColor,
-                                  ),
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          controller.pickImage();
+                        },
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            ShadowWidget(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  color: Colors.grey.shade300,
                                 ),
+                                height: 300,
+                                width: 300,
+                                child: (controller.imagePath == null)
+                                    ? null
+                                    : Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(8.0),
+                                            child: Image.file(
+                                              File(controller.imagePath!),
+                                              height: 280,
+                                              width: 280,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                               ),
                             ),
-                    ),
+                            Icon(
+                              Icons.camera_enhance_rounded,
+                              color: AppColor.tealColor.withOpacity(
+                                  controller.imagePath != null ? 0.6 : 1),
+                              size: 120,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 42),
+                        child: AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 300),
+                          child: controller.imagePath == null
+                              ? const SizedBox()
+                              : InkWell(
+                                  onTap: () {
+                                    controller.showResult(context);
+                                  },
+                                  child: const ShadowWidget.image(
+                                    child: IgnorePointer(
+                                      child: AppButton(
+                                        title: 'CONTINUE',
+                                        icon: Padding(
+                                          padding: EdgeInsets.only(top: 6),
+                                          child: Icon(
+                                            Icons.arrow_forward_rounded,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        backgroundColor: AppColor.pinkColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      }, valueListenable: controller.notifier,
     );
   }
 }
